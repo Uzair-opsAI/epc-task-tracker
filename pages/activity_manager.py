78261c65b84cb12671f5748ae3fa94a0b2b21c01
Uzair-opsAI <uzair.saiyed786@gmail.com>
2026-07-27 17:49:09 +0530

    Update activity_manager.py

--- pages/activity_manager.py
+++ pages/activity_manager.py
@@ -1,1 +1,16 @@
-print("activity")
+import streamlit as st
+from database import get_tasks
+
+def show():
+
+    st.title("📋 Activity Manager")
+
+    st.markdown("---")
+
+    tasks = get_tasks()
+
+    st.dataframe(
+        tasks,
+        use_container_width=True,
+        hide_index=True
+    )
