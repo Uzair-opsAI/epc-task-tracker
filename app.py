032160e8783900f0ed2cbd64bac4902ebfaa3ee3
Uzair-opsAI <uzair.saiyed786@gmail.com>
2026-07-29 14:46:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from database import get_projects
 from database import get_employees
 from pages.activity_manager import show as activity_page
 from pages.analytics import show as analytics_page
+from pages.dashboard import show as dashboard_page
 # -------------------------------------------------
 # Page Configuration
 # -------------------------------------------------
